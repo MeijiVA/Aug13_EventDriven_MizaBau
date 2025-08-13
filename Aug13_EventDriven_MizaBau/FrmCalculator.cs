5afaac0af7a8428a00cccf492b99ea9393acac26
MeijiVA <mizaelantonbautista@gmail.com>
2025-08-13 19:58:54 +0800

    Added Note

--- Aug13_EventDriven_MizaBau/FrmCalculator.cs
+++ Aug13_EventDriven_MizaBau/FrmCalculator.cs
@@ -12,7 +12,7 @@ using static Aug13_EventDriven_MizaBau.FrmCalculator;
 
 namespace Aug13_EventDriven_MizaBau
 {
-
+    //STEP 4. DELEGATE
     public delegate T Formula<T>(T arg1, T arg2);
     public class CalculatorClass
     {
@@ -21,28 +21,25 @@ namespace Aug13_EventDriven_MizaBau
         public Formula<double> info;
 
         //STEP 6. METHOD
-        public static double GetSum(double num1, double num2)
+        public double GetSum(double num1, double num2)
         {
             return num1 + num2;
         }
-        public static double GetDifference(double num1, double num2)
+        public double GetDifference(double num1, double num2)
         {
             return num1 - num2;
         }
         //STEP 12. CHALLENGE EXERCISE GetProduct GetQuotient
-        public static double GetProduct(double num1, double num2)
+        public double GetProduct(double num1, double num2)
         {
             return num1 * num2;
         }
 
-        public static double GetQuotient(double num1, double num2)
+        public double GetQuotient(double num1, double num2)
         {
             return num1 / num2;
         }
-    }
-    //STEP 4. CALCULATOR CLASS & DELEGATE
-    public partial class FrmCalculator : Form
-    {
+
 
 
         //Step 7. EVENT
@@ -59,20 +56,23 @@ namespace Aug13_EventDriven_MizaBau
 
         }
 
+    }
+    //STEP 4. CALCULATOR CLASS & DELEGATE
+    public partial class FrmCalculator : Form
+    {
 
         public FrmCalculator()
         {
             InitializeComponent();
-            //STEP 8. INSTANTIATE 
-            CalculatorClass cal;
-            cal = new CalculatorClass();
         }
 
         //STEP 9. VARIABLES
         double num1, num2;
         private void btnEqual_Click(object sender, EventArgs e)
         {
-
+            //STEP 8. INSTANTIATE 
+            CalculatorClass cal;
+            cal = new CalculatorClass();
             //STEP 10. Get Value
             num1 = Convert.ToDouble(txtBoxInput1.Text);
             num2 = Convert.ToDouble(txtBoxInput2.Text);
@@ -81,18 +81,27 @@ namespace Aug13_EventDriven_MizaBau
             switch (cbOperator.Text)
             {
                 case "+":
-                    CalculateEvent += new Formula<double>(CalculatorClass.GetSum);
-                    lblDisplayTotal.Text = CalculatorClass.GetSum(num1, num2).ToString();
-                    CalculateEvent -= new Formula
+                    cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                    lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetSum);
                     break;
                 case "-":
+                    cal.CalculateEvent += new Formula<double>(cal.GetDifference);
+                    lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
                     break;
                 case "*":
+                    cal.CalculateEvent += new Formula<double>(cal.GetProduct);
+                    lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetProduct);
                     break;
                 case "/":
+                    cal.CalculateEvent += new Formula<double>(cal.GetQuotient);
+                    lblDisplayTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetQuotient);
                     break;
                 default:
-
+                    MessageBox.Show("Unknown Operator");
                     break;
 
             }
